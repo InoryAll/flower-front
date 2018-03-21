e7be0ac089a0348b7c9241a553b5fd4a8658b383
Jie ren Tian <110873204@qq.com>
2018-03-21 17:36:21 +0800

    鲜花销售主页logo+search区域开发.

---
+++ src/user/index/search/Search.jsx
@@ -0,0 +1,28 @@
+/**
+ * 鲜花销售主页logo+search区域
+ * Created by tianrenjie on 2018/3/21
+ */
+import React, { PropTypes } from 'react';
+import { Row, Col } from 'antd';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="logo">
+        <Row className="logo-row">
+          <Col>
+            <div></div>
+          </Col>
+          <Col>
+            <div></div>
+          </Col>
+          <Col>
+            <div></div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default Search;
