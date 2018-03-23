9abd80912baf953e6b001c3414e77a4d55ffe376
Jie ren Tian <110873204@qq.com>
2018-03-23 17:17:15 +0800

    鲜花销售系统主页的cart组件的完成.

--- src/user/index/search/cart/Cart.jsx
+++ src/user/index/search/cart/Cart.jsx
@@ -7,67 +7,83 @@ import { Row, Col, Icon } from 'antd';
 import { If } from 'jsx-control-statements';
 import { Link } from 'react-router';
 import Goods from './goods/Goods';
+import '../../../../animation/animation.less';
 import './Cart.less';
 
 class Cart extends React.Component {
   static propsTypes = {};
+  state = {
+    visible: false,
+  };
+  handleMouseEnter = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  handleMouseLeave = () => {
+    this.setState({
+      visible: false,
+    });
+  };
   render() {
     return (
       <div className="cart clearfix">
         <Row className="cart-row-button">
           <Col>
-            <div className="cart-row-button-box">
+            <div className={this.state.visible ? 'cart-row-button-box cart-row-button-box-active' : 'cart-row-button-box cart-row-button-box-negative'} onMouseEnter={ this.handleMouseEnter } onMouseLeave={ this.handleMouseLeave }>
               <Icon type="shopping-cart" className="cart-row-button-box-cart" />
               <span className="cart-row-button-box-text">购物车结算</span>
               <Icon type="down" className="cart-row-button-box-arrow" />
             </div>
           </Col>
         </Row>
-        <Row className="cart-row-content">
-          <Col>
-            <div className="cart-row-content-box">
-              <Row className="cart-row-content-box-row-title">
-                <Col>
-                  <h4 className="cart-row-content-box-row-title-h4">最新加入的商品</h4>
-                </Col>
-              </Row>
-              <Row className="cart-row-content-box-row-list">
-                <Col>
-                  <div className="cart-row-content-box-row-list-box">
-                    <If condition={ 0 || false }>
-                      <div className="no-items">
-                        <span>您的购物车中暂无商品，赶快选择心爱的商品吧！</span>
-                      </div>
-                    </If>
-                    <If condition={ 1 || true }>
-                      <Goods />
-                      <Goods />
-                      <Goods />
-                    </If>
-                  </div>
-                </Col>
-              </Row>
-              <Row className="cart-row-content-box-row-bottom">
-                <Col>
-                  <div className="cart-row-content-box-row-bottom-box">
-                    <Row>
-                      <Col className="clearfix">
-                        <If condition={ 1 || true }>
-                          <p className="check-items">共<span className="check-items-count">2</span>种商品  总计金额：<strong className="check-items-price">¥439.00</strong></p>
-                        </If>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col className="clearfix">
-                        <Link to="" className="checkout-cart">结算购物车中的商品</Link>
-                      </Col>
-                    </Row>
-                  </div>
-                </Col>
-              </Row>
-            </div>
-          </Col>
-        </Row>
+        <If condition={this.state.visible}>
+          <Row className="cart-row-content">
+            <Col className="clearfix">
+              <div className="cart-row-content-box">
+                <Row className="cart-row-content-box-row-title">
+                  <Col>
+                    <h4 className="cart-row-content-box-row-title-h4">最新加入的商品</h4>
+                  </Col>
+                </Row>
+                <Row className="cart-row-content-box-row-list">
+                  <Col>
+                    <div className="cart-row-content-box-row-list-box">
+                      <If condition={ 0 || false }>
+                        <div className="no-items">
+                          <span>您的购物车中暂无商品，赶快选择心爱的商品吧！</span>
+                        </div>
+                      </If>
+                      <If condition={ 1 || true }>
+                        <Goods />
+                        <Goods />
+                        <Goods />
+                      </If>
+                    </div>
+                  </Col>
+                </Row>
+                <Row className="cart-row-content-box-row-bottom">
+                  <Col>
+                    <div className="cart-row-content-box-row-bottom-box">
+                      <Row>
+                        <Col className="clearfix">
+                          <If condition={ 1 || true }>
+                            <p className="check-items">共<span className="check-items-count">2</span>种商品  总计金额：<strong className="check-items-price">¥439.00</strong></p>
+                          </If>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col className="clearfix">
+                          <Link to="" className="checkout-cart">结算购物车中的商品</Link>
+                        </Col>
+                      </Row>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </If>
       </div>
     );
   }
