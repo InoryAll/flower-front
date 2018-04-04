5d620f172a8b20541a10ca9f24e5c7ea4c9c9a8e
Jie Ren tian <110873204@qq.com>
2018-04-04 16:22:27 +0800

    鲜花销售系统主页的Promote组件开发.

---
+++ src/user/index/promote/Promote.jsx
@@ -0,0 +1,18 @@
+/**
+ * 鲜花销售主页推广
+ * Created by tianrenjie on 2018/4/4
+ */
+import React, { PropTypes } from 'react';
+import { Row, Col } from 'antd';
+
+class Promote extends React.Component {
+  render() {
+    return (
+      <div className="promote">
+        111
+      </div>
+    );
+  }
+}
+
+export default Promote;
