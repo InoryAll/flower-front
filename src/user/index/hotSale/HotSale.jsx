0f7b9cd810555552ee9e456423764c782ad43481
Jie Ren tian <110873204@qq.com>
2018-04-08 14:20:32 +0800

    鲜花系统首页的HotSaler图片资源加载.

--- src/user/index/hotSale/HotSale.jsx
+++ src/user/index/hotSale/HotSale.jsx
@@ -4,9 +4,21 @@
  */
 import React, { PropTypes } from 'react';
 import { Row, Col } from 'antd';
+import img1 from '../../../../static/img/hotsale/1.jpg';
+import img2 from '../../../../static/img/hotsale/1_1.jpg';
+import img3 from '../../../../static/img/hotsale/1_2.jpg';
+import img4 from '../../../../static/img/hotsale/1_3.jpg';
+import img5 from '../../../../static/img/hotsale/1_4.jpg';
+import img6 from '../../../../static/img/hotsale/2.jpg';
+import img7 from '../../../../static/img/hotsale/2_1.jpg';
+import img8 from '../../../../static/img/hotsale/2_2.jpg';
 import './HotSale.less';
 
 class HotSale extends React.Component {
+  static propTypes = {};
+  state = {
+    imgArr: [img1, img2, img3, img4, img5, img6, img7, img8],
+  };
   render() {
     return (
       <div className="hotsale">
