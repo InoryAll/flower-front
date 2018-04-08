0ab3ade8ba8a9f4d77094eaa11cf432cef9edea8
Jie Ren tian <110873204@qq.com>
2018-04-08 15:20:34 +0800

    鲜花系统首页的HotSaler组件的完成.

--- src/user/index/hotSale/HotSale.jsx
+++ src/user/index/hotSale/HotSale.jsx
@@ -4,6 +4,7 @@
  */
 import React, { PropTypes } from 'react';
 import { Row, Col } from 'antd';
+import { Link } from 'react-router';
 import img1 from '../../../../static/img/hotsale/1.jpg';
 import img2 from '../../../../static/img/hotsale/1_1.jpg';
 import img3 from '../../../../static/img/hotsale/1_2.jpg';
@@ -27,8 +28,57 @@ class HotSale extends React.Component {
             <h3 className="hotsale-h">正在热卖<span className="hotsale-h-span">精选热销款式</span></h3>
           </Col>
         </Row>
-        <Row>
-          <Col></Col>
+        <Row className="hotsale-detail-row">
+          <Col span={4}>
+            <Link className="hotsale-special-link" to="#"><img className="hotsale-special-link-img" src={this.state.imgArr[0]} alt="花之韵" /></Link>
+          </Col>
+          <Col span={11}>
+            <ul className="hotsale-ul">
+              <li className="hotsale-ul-li">
+                <Link className="hotsale-link" to="#" >
+                  <p className="hotsale-link-p">携手一生</p>
+                  <img className="hotsale-link-img" src={this.state.imgArr[1]} alt="" />
+                </Link>
+              </li>
+              <li className="hotsale-ul-li">
+                <Link className="hotsale-link" to="" >
+                  <p className="hotsale-link-p">思念是一种病</p>
+                  <img className="hotsale-link-img" src={this.state.imgArr[2]} alt="" />
+                </Link>
+              </li>
+              <li className="hotsale-ul-li">
+                <Link className="hotsale-link" to="" >
+                  <p className="hotsale-link-p">爱的守护神</p>
+                  <img className="hotsale-link-img" src={this.state.imgArr[3]} alt="" />
+                </Link>
+              </li>
+              <li className="hotsale-ul-li">
+                <Link className="hotsale-link" to="" >
+                  <p className="hotsale-link-p">你是我的璀璨星光</p>
+                  <img className="hotsale-link-img" src={this.state.imgArr[4]} alt="" />
+                </Link>
+              </li>
+            </ul>
+          </Col>
+          <Col span={4}>
+            <Link className="hotsale-special-link" to="#"><img className="hotsale-special-link-img" src={this.state.imgArr[5]} alt="花之韵" /></Link>
+          </Col>
+          <Col span={5}>
+            <ul className="hotsale-ul hotsale-ul-vertical">
+              <li className="hotsale-ul-li">
+                <Link className="hotsale-link" to="" >
+                  <p className="hotsale-link-p">风的光阴</p>
+                  <img className="hotsale-link-img" src={this.state.imgArr[6]} alt="" />
+                </Link>
+              </li>
+              <li className="hotsale-ul-li">
+                <Link className="hotsale-link" to="" >
+                  <p className="hotsale-link-p">梦中婚礼</p>
+                  <img className="hotsale-link-img" src={this.state.imgArr[7]} alt="" />
+                </Link>
+              </li>
+            </ul>
+          </Col>
         </Row>
       </div>
     );
