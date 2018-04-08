9638a75e5a31af906525acede410226ce0b94a85
Jie Ren tian <110873204@qq.com>
2018-04-08 17:25:14 +0800

    鲜花系统首页的FirstFloor组件的开发.

--- src/user/index/firstFloor/FirstFloor.jsx
+++ src/user/index/firstFloor/FirstFloor.jsx
@@ -9,20 +9,28 @@ import './FirstFloor.less';
 
 class FirstFloor extends React.Component {
   static propTypes = {};
+  state = {
+    menuItem: 'default',
+  };
+  onSearch = (attribute, value) => {
+    console.log(attribute, value);
+    this.setState({
+      menuItem: value,
+    });
+  };
   render() {
     return (
-      <div>
-        FirstFloor
+      <div className="firstfloor">
         <Row>
           <Col>
-            <h3>1F 玫瑰花</h3>
-            <ul>
-              <li><Link to="#">精选热卖</Link></li>
-              <li><Link to="#">红玫瑰</Link></li>
-              <li><Link to="#">白玫瑰</Link></li>
-              <li><Link to="#">粉玫瑰</Link></li>
-              <li><Link to="#">香槟玫瑰</Link></li>
-              <li><Link to="#">蓝色妖姬</Link></li>
+            <h3 className="firstfloor-h">1F 玫瑰花</h3>
+            <ul className="firstfloor-title-ul">
+              <li className="firstfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'default'); }} className={this.state.menuItem === 'default' ? 'firstfloor-title-ul-li-a active' : 'firstfloor-title-ul-li-a'} to="#">精选热卖</Link></li>
+              <li className="firstfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'redRose'); }} className={this.state.menuItem === 'redRose' ? 'firstfloor-title-ul-li-a active' : 'firstfloor-title-ul-li-a'} to="#">红玫瑰</Link></li>
+              <li className="firstfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'whiteRose'); }} className={this.state.menuItem === 'whiteRose' ? 'firstfloor-title-ul-li-a active' : 'firstfloor-title-ul-li-a'} to="#">白玫瑰</Link></li>
+              <li className="firstfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'pinkRose'); }} className={this.state.menuItem === 'pinkRose' ? 'firstfloor-title-ul-li-a active' : 'firstfloor-title-ul-li-a'} to="#">粉玫瑰</Link></li>
+              <li className="firstfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'champagneRose'); }} className={this.state.menuItem === 'champagneRose' ? 'firstfloor-title-ul-li-a active' : 'firstfloor-title-ul-li-a'} to="#">香槟玫瑰</Link></li>
+              <li className="firstfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'blueRose'); }} className={this.state.menuItem === 'blueRose' ? 'firstfloor-title-ul-li-a active' : 'firstfloor-title-ul-li-a'} to="#">蓝色妖姬</Link></li>
             </ul>
           </Col>
         </Row>
