0e100b4c6595f98b8cda89daa3f4dd2e7054082c
Jie Ren tian <110873204@qq.com>
2018-04-11 15:47:23 +0800

    鲜花系统首页的FirstFloor组件的完成.

--- src/user/index/firstFloor/FirstFloor.jsx
+++ src/user/index/firstFloor/FirstFloor.jsx
@@ -7,9 +7,14 @@ import { Row, Col } from 'antd';
 import { Link } from 'react-router';
 import FloorSpecific from './floorSpecific/FloorSpecific';
 import FloorBasic from './floorBasic/FloorBasic';
-import img from '../../../../static/img/floorSpecific/1.jpg';
-import basic1 from '../../../../static/img/floorBasic/1.jpg';
-import basic2 from '../../../../static/img/floorBasic/2.jpg';
+import own from '../../../../static/img/firstFloor/1.jpg';
+import specific1 from '../../../../static/img/firstFloor/floorSpecific/1.jpg';
+import basic1 from '../../../../static/img/firstFloor/floorBasic/1.jpg';
+import basic2 from '../../../../static/img/firstFloor/floorBasic/2.jpg';
+import basic3 from '../../../../static/img/firstFloor/floorBasic/3.jpg';
+import basic4 from '../../../../static/img/firstFloor/floorBasic/4.jpg';
+import basic5 from '../../../../static/img/firstFloor/floorBasic/5.jpg';
+import basic6 from '../../../../static/img/firstFloor/floorBasic/6.jpg';
 
 import './FirstFloor.less';
 
@@ -43,7 +48,7 @@ class FirstFloor extends React.Component {
         <Row>
           <Col span={4}>
             <FloorSpecific
-              top={{ img }}
+              top={{ img: specific1 }}
               middle={{ bg: '#a6ddc9', url: '#', text: '新品上市' }}
               bottom={{ bg: '#caf4e5',
                 data: [{
@@ -69,7 +74,7 @@ class FirstFloor extends React.Component {
                 }] }}
             />
           </Col>
-          <Col span={4}>
+          <Col span={5}>
             <FloorBasic
               top={{ url: '#', img: basic1, text: '梦中有你' }}
               bottom={{ nowPrice: '￥179.00', prePrice: '￥129.00' }}
@@ -77,6 +82,29 @@ class FirstFloor extends React.Component {
             <FloorBasic
               top={{ url: '#', img: basic2, text: '花开月正圆' }}
               bottom={{ nowPrice: '￥229.00', prePrice: '￥386.00' }}
+            />
+          </Col>
+          <Col span={7}>
+            <img className="firstfloor-img" src={own} alt="花之韵" />
+          </Col>
+          <Col span={4}>
+            <FloorBasic
+              top={{ url: '#', img: basic3, text: '谱写幸福' }}
+              bottom={{ nowPrice: '￥229.00', prePrice: '￥329.00' }}
+            />
+            <FloorBasic
+              top={{ url: '#', img: basic4, text: '完美爱人' }}
+              bottom={{ nowPrice: '￥249.00', prePrice: '￥369.00' }}
+            />
+          </Col>
+          <Col span={4}>
+            <FloorBasic
+              top={{ url: '#', img: basic5, text: '我的宠妃' }}
+              bottom={{ nowPrice: '￥300.00', prePrice: '￥436.00' }}
+            />
+            <FloorBasic
+              top={{ url: '#', img: basic6, text: '微微一笑很倾城' }}
+              bottom={{ nowPrice: '￥289.00', prePrice: '￥421.00' }}
             />
           </Col>
         </Row>
