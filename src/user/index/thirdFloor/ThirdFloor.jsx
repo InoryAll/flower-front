50f3b933e33c9668792670a58cee2871f46cc353
Jie Ren tian <110873204@qq.com>
2018-04-12 16:50:35 +0800

    鲜花系统首页的ThirdFloor组件的完成.

--- src/user/index/thirdFloor/ThirdFloor.jsx
+++ src/user/index/thirdFloor/ThirdFloor.jsx
@@ -8,13 +8,13 @@ import { Link } from 'react-router';
 import FloorSpecific from '../firstFloor/floorSpecific/FloorSpecific';
 import FloorBasic from '../firstFloor/floorBasic/FloorBasic';
 import own from '../../../../static/img/thirdFloor/1.jpg';
-import specific1 from '../../../../static/img/secondFloor/floorSpecific/1.jpg';
-import basic1 from '../../../../static/img/secondFloor/floorBasic/1.jpg';
-import basic2 from '../../../../static/img/secondFloor/floorBasic/2.jpg';
-import basic3 from '../../../../static/img/secondFloor/floorBasic/3.jpg';
-import basic4 from '../../../../static/img/secondFloor/floorBasic/4.jpg';
-import basic5 from '../../../../static/img/secondFloor/floorBasic/5.jpg';
-import basic6 from '../../../../static/img/secondFloor/floorBasic/6.jpg';
+import specific1 from '../../../../static/img/thirdFloor/floorSpecific/1.jpg';
+import basic1 from '../../../../static/img/thirdFloor/floorBasic/1.jpg';
+import basic2 from '../../../../static/img/thirdFloor/floorBasic/2.jpg';
+import basic3 from '../../../../static/img/thirdFloor/floorBasic/3.jpg';
+import basic4 from '../../../../static/img/thirdFloor/floorBasic/4.jpg';
+import basic5 from '../../../../static/img/thirdFloor/floorBasic/5.jpg';
+import basic6 from '../../../../static/img/thirdFloor/floorBasic/6.jpg';
 
 import './ThirdFloor.less';
 
@@ -36,12 +36,12 @@ class ThirdFloor extends React.Component {
           <Col className="clearfix thirdfloor-title">
             <h3 className="thirdfloor-h">3F 礼盒鲜花</h3>
             <ul className="thirdfloor-title-ul">
-              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'default'); }} className={this.state.menuItem === 'default' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">精选热卖</Link></li>
-              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'whiteLily'); }} className={this.state.menuItem === 'whiteLily' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">白百何</Link></li>
-              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'pinkLily'); }} className={this.state.menuItem === 'pinkLily' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">粉百合</Link></li>
-              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'carnations'); }} className={this.state.menuItem === 'carnations' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">康乃馨</Link></li>
-              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'sunflower'); }} className={this.state.menuItem === 'sunflower' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">向日葵</Link></li>
-              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('material', 'tulip'); }} className={this.state.menuItem === 'tulip' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">郁金香</Link></li>
+              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('holiday', 'default'); }} className={this.state.menuItem === 'default' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">精选热卖</Link></li>
+              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('holiday', 'chineseValentine'); }} className={this.state.menuItem === 'chineseValentine' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">七夕</Link></li>
+              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('holiday', 'valentine'); }} className={this.state.menuItem === 'valentine' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">情人节</Link></li>
+              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('holiday', 'bachelor'); }} className={this.state.menuItem === 'bachelor' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">光棍节</Link></li>
+              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('holiday', 'christmas'); }} className={this.state.menuItem === 'christmas' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">圣诞节</Link></li>
+              <li className="thirdfloor-title-ul-li"><Link onClick={() => { this.onSearch('holiday', 'teacher'); }} className={this.state.menuItem === 'teacher' ? 'thirdfloor-title-ul-li-a active' : 'thirdfloor-title-ul-li-a'} to="#">教师节</Link></li>
             </ul>
           </Col>
         </Row>
@@ -49,39 +49,39 @@ class ThirdFloor extends React.Component {
           <Col span={4}>
             <FloorSpecific
               top={{ img: specific1 }}
-              middle={{ bg: '#a6ddc9', url: '#', text: '新品上市' }}
-              bottom={{ bg: '#caf4e5',
+              middle={{ bg: '#cda5e6', url: '#', text: '礼盒鲜花' }}
+              bottom={{ bg: '#f4e3ff',
                 data: [{
-                  text: '白百何',
-                  attribute: 'material',
-                  value: 'whiteLily',
+                  text: '七夕',
+                  attribute: 'holiday',
+                  value: 'chineseValentine',
                 }, {
-                  text: '粉百合',
-                  attribute: 'material',
-                  value: 'pinkLily',
+                  text: '情人节',
+                  attribute: 'holiday',
+                  value: 'valentine',
                 }, {
-                  text: '康乃馨',
-                  attribute: 'material',
-                  value: 'carnations',
+                  text: '光棍节',
+                  attribute: 'holiday',
+                  value: 'bachelor',
                 }, {
-                  text: '向日葵',
-                  attribute: 'material',
-                  value: 'sunflower',
+                  text: '圣诞节',
+                  attribute: 'holiday',
+                  value: 'christmas',
                 }, {
-                  text: '郁金香',
-                  attribute: 'material',
-                  value: 'tulip',
+                  text: '教师节',
+                  attribute: 'holiday',
+                  value: 'teacher',
                 }] }}
             />
           </Col>
           <Col span={5}>
             <FloorBasic
-              top={{ url: '#', img: basic1, text: '相依相携' }}
-              bottom={{ nowPrice: '￥229.00', prePrice: '￥359.00' }}
+              top={{ url: '#', img: basic1, text: '清水茉莉' }}
+              bottom={{ nowPrice: '￥189.00', prePrice: '￥289.00' }}
             />
             <FloorBasic
-              top={{ url: '#', img: basic2, text: '天使之心' }}
-              bottom={{ nowPrice: '￥249.00', prePrice: '￥379.00' }}
+              top={{ url: '#', img: basic2, text: '爱入佳境' }}
+              bottom={{ nowPrice: '￥308.00', prePrice: '￥446.00' }}
             />
           </Col>
           <Col span={7}>
@@ -89,22 +89,22 @@ class ThirdFloor extends React.Component {
           </Col>
           <Col span={4}>
             <FloorBasic
-              top={{ url: '#', img: basic3, text: '专属天使' }}
-              bottom={{ nowPrice: '￥299.00', prePrice: '￥439.00' }}
+              top={{ url: '#', img: basic3, text: '这样爱你' }}
+              bottom={{ nowPrice: '￥249.00', prePrice: '￥369.00' }}
             />
             <FloorBasic
-              top={{ url: '#', img: basic4, text: '真爱旅程' }}
-              bottom={{ nowPrice: '￥293.00', prePrice: '￥426.00' }}
+              top={{ url: '#', img: basic4, text: '浪漫爱' }}
+              bottom={{ nowPrice: '￥300.00', prePrice: '￥399.00' }}
             />
           </Col>
           <Col span={4}>
             <FloorBasic
-              top={{ url: '#', img: basic5, text: '亲密无间' }}
-              bottom={{ nowPrice: '￥300.00', prePrice: '￥436.00' }}
+              top={{ url: '#', img: basic5, text: '签约幸福' }}
+              bottom={{ nowPrice: '￥318.00', prePrice: '￥459.00' }}
             />
             <FloorBasic
-              top={{ url: '#', img: basic6, text: '唯爱一生' }}
-              bottom={{ nowPrice: '￥509.00', prePrice: '￥799.00' }}
+              top={{ url: '#', img: basic6, text: '曼妙时光' }}
+              bottom={{ nowPrice: '￥698.00', prePrice: '￥999.00' }}
             />
           </Col>
         </Row>
