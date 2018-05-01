ccac5d6822cf568230d73b41dc458ad31f7dd86d
Jie ren Tian <110873204@qq.com>
2018-05-01 16:36:29 +0800

    鲜花系统注册页Regedit组件开发.

--- src/router.jsx
+++ src/router.jsx
@@ -6,12 +6,14 @@ import React from 'react';
 import { browserHistory, Router, Route, IndexRoute } from 'react-router';
 import Index from './user/index/Index';
 import Login from './user/login/Login';
+import Regedit from './user/regedit/Regedit';
 
 export const router = (
   <Router history={browserHistory}>
     <Route path="/">
       <IndexRoute component={Index} />
       <Route path="login" component={Login} />
+      <Route path="regedit" component={Regedit} />
     </Route>
   </Router>
 );
---
+++ src/user/regedit/Regedit.jsx
@@ -0,0 +1,17 @@
+/**
+ * 鲜花销售注册页
+ * Created by tianrenjie on 2018/4/27
+ */
+import React, { PropTypes } from 'react';
+
+class Regedit extends React.Component {
+  render() {
+    return (
+      <div>
+        Regedit
+      </div>
+    );
+  }
+}
+
+export default Regedit;
