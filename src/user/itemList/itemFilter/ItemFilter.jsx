1e7736a0ff05badce0b3bbd1f4c4afd4bf6e93f9
Jie ren Tian <110873204@qq.com>
2018-05-03 22:41:14 +0800

    鲜花系统商品页ItemFilter组件开发.

--- src/user/itemList/itemFilter/ItemFilter.jsx
+++ src/user/itemList/itemFilter/ItemFilter.jsx
@@ -4,7 +4,7 @@
  */
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import { Row, Col } from 'antd';
+import { Row, Col, Icon } from 'antd';
 import './ItemFilter.less';
 
 class ItemFilter extends React.Component {
@@ -18,11 +18,16 @@ class ItemFilter extends React.Component {
       filter: [...new Set([...this.state.filter, value])],
     });
   };
+  handleDeleteClick = (value) => {
+    this.setState({
+      filter: this.state.filter.filter(item => item !== value),
+    });
+  };
   render() {
     const { filter } = this.state;
     const selectItem = [];
     this.state.filter.map((item, index) => {
-      selectItem.push(<li key={index}>{item}</li>);
+      selectItem.push(<li className="itemfilter-selected-ul-li" key={index}><Link onClick={() => { this.handleDeleteClick(item); }} className="itemfilter-selected-link">{item}<span className="itemfilter-selected-icon"><Icon type="close" /></span></Link></li>);
       return true;
     });
     return (
@@ -35,38 +40,32 @@ class ItemFilter extends React.Component {
           </Row>
           <Row>
             <Col>
-              <h3 className="itemfilter-title">您已选择</h3>
-              <ul>
-                {selectItem}
-              </ul>
+              <div className="itemfilter-selected clearfix">
+                <span className="itemfilter-selected-span">您已选择：</span>
+                <ul className="itemfilter-selected-ul">
+                  {selectItem}
+                </ul>
+              </div>
             </Col>
           </Row>
           <Row>
             <Col>
               <ul className="itemfilter-ul">
-                <li className="itemfilter-ul-li">
-                  <span className="itemfilter-ul-li-span">用途</span>
+                <li className="itemfilter-ul-li clearfix">
+                  <span className="itemfilter-ul-li-span">用途：</span>
                   <ul className="itemfilter-ul-li-ul">
                     <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'love'); }} className={ filter.indexOf('love') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>爱情鲜花</Link></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
-                    <li className="itemfilter-ul-li-ul-li"><Link className="itemfilter-ul-li-ul-li-link"/></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'business'); }} className={ filter.indexOf('business') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>商务用花</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'birthday'); }} className={ filter.indexOf('birthday') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>生日鲜花</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'friendship'); }} className={ filter.indexOf('friendship') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>友情鲜花</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'greetings'); }} className={ filter.indexOf('greetings') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>老师长辈</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'wedding'); }} className={ filter.indexOf('wedding') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>求婚婚庆</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'apology'); }} className={ filter.indexOf('apology') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>道歉鲜花</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'sympathy'); }} className={ filter.indexOf('sympathy') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>探病慰问</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'congratulation'); }} className={ filter.indexOf('congratulation') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>祝福庆贺</Link></li>
+                    <li className="itemfilter-ul-li-ul-li"><Link onClick={() => { this.onSearch('usage', 'death'); }} className={ filter.indexOf('death') > -1 ? 'itemfilter-ul-li-ul-li-link itemfilter-active' : 'itemfilter-ul-li-ul-li-link' }>祭奠</Link></li>
                   </ul>
                 </li>
-                <li className="itemfilter-ul-li"></li>
-                <li className="itemfilter-ul-li"></li>
-                <li className="itemfilter-ul-li"></li>
-                <li className="itemfilter-ul-li"></li>
-                <li className="itemfilter-ul-li"></li>
-                <li className="itemfilter-ul-li"></li>
-                <li className="itemfilter-ul-li"></li>
-                <li className="itemfilter-ul-li"></li>
               </ul>
             </Col>
           </Row>
