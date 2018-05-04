a04979e34dc10dbb926b3a67fe24a5908e1c1329
Jie ren Tian <110873204@qq.com>
2018-05-04 13:20:58 +0800

    鲜花系统商品页ItemSelf组件开发.

--- src/user/itemList/itemList/ItemPage.jsx
+++ src/user/itemList/itemList/ItemPage.jsx
@@ -1,15 +1,21 @@
 /**
  * 鲜花销售商品列表页面商品列表
- * Created by tianrenjie on 2018/5/3
+ * Created by tianrenjie on 2018/5/4
  */
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import { Row, Col } from 'antd';
+import './ItemPage.less';
 
 class ItemPage extends React.Component {
   static propTypes = {};
   render() {
     return (
-      <div>
-        ...
+      <div className="itempage">
+        <div className="itempage-box">
+          <div className="itempage-box-list">....</div>
+          <div className="itempage-box-pagination">....</div>
+        </div>
       </div>
     );
   }
---
+++ src/user/itemList/itemList/itemSelf/ItemSelf.jsx
@@ -0,0 +1,20 @@
+/**
+ * 鲜花销售商品列表页面商品个体
+ * Created by tianrenjie on 2018/5/4
+ */
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import './ItemSelf.less';
+
+class ItemSelf extends React.Component {
+  static propTypes = {};
+  render() {
+    return (
+      <div className="itemself">
+        ...
+      </div>
+    );
+  }
+}
+
+export default ItemSelf;
