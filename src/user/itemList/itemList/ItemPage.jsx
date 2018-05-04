636119eaf03ff1f78efd643ed6bde49ad43e55d2
Jie ren Tian <110873204@qq.com>
2018-05-04 20:24:07 +0800

    鲜花系统商品页ItemSelf组件开发.

--- src/user/itemList/itemList/ItemPage.jsx
+++ src/user/itemList/itemList/ItemPage.jsx
@@ -4,17 +4,25 @@
  */
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import { Row, Col } from 'antd';
+import { Row, Col, Pagination } from 'antd';
 import './ItemPage.less';
+import ItemSelf from './itemSelf/ItemSelf';
 
 class ItemPage extends React.Component {
   static propTypes = {};
+  onShowSizeChange = (current, pageSize) => {
+    console.log(current, pageSize);
+  };
   render() {
     return (
       <div className="itempage">
         <div className="itempage-box">
-          <div className="itempage-box-list">....</div>
-          <div className="itempage-box-pagination">....</div>
+          <div className="itempage-box-list">
+            <ItemSelf />
+          </div>
+          <div className="itempage-box-pagination">
+            <Pagination showSizeChanger onShowSizeChange={this.onShowSizeChange} defaultCurrent={3} total={500} />
+          </div>
         </div>
       </div>
     );
--- src/user/itemList/itemList/itemSelf/ItemSelf.jsx
+++ src/user/itemList/itemList/itemSelf/ItemSelf.jsx
@@ -4,6 +4,8 @@
  */
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
+import { Row, Col, Icon } from 'antd';
+import img from '../../../../../static/img/item/1.jpg';
 import './ItemSelf.less';
 
 class ItemSelf extends React.Component {
@@ -11,7 +13,67 @@ class ItemSelf extends React.Component {
   render() {
     return (
       <div className="itemself">
-        ...
+        <Row>
+          <Col>
+            <div className="itemself-pic">
+              <Link to="" className="itemself-pic-link">
+                <img src={img} alt="花之韵" className="itemself-pic-link-img" />
+              </Link>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="itemself-intro">
+              <Row>
+                <Col>
+                  <div className="itemself-des">
+                    <Link to="">
+                      <p className="itemself-des-title">携手一生</p>
+                      <p className="itemself-des-detail">11枝优质红玫瑰搭配绿叶，满天星，随机赠送2只小熊</p>
+                    </Link>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="itemself-price">
+                    <span className="itemself-price-now">￥179</span>
+                    <span className="itemself-price-pre">￥299</span>
+                    <div className="itemself-price-star">
+                      <Icon type="star" />
+                      <Icon type="star" />
+                      <Icon type="star" />
+                      <Icon type="star" />
+                      <Icon type="star" />
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={12}>
+                  <div className="itemself-count">
+                    <p><Link to="">8156</Link></p>
+                    <p>商品销量</p>
+                  </div>
+                </Col>
+                <Col span={12}>
+                  <div className="itemself-comment">
+                    <p><Link to="">7318</Link></p>
+                    <p>用户评论</p>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="itemself-cart-add">
+                    <button><span><Icon type="shopping-cart" /></span>加入购物车</button>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
