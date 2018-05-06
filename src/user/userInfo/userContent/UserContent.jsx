13a967c1b37f66afa003e0ecdc3c55fa8c9f109d
Jie ren Tian <110873204@qq.com>
2018-05-06 23:11:02 +0800

    鲜花系统个人中心页UserContent组件开发.

---
+++ src/user/userInfo/userContent/UserContent.jsx
@@ -0,0 +1,18 @@
+/**
+ * 鲜花销售个人中心页主要内容
+ * Created by tianrenjie on 2018/5/6
+ */
+import React, { PropTypes } from 'react';
+
+class UserContent extends React.Component {
+  static propTypes = {};
+  render() {
+    return (
+      <div className="usercontent">
+        ...
+      </div>
+    );
+  }
+}
+
+export default UserContent;
