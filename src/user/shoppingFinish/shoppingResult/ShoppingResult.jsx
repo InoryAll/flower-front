303b0eb733a24b137e3fa06df36b67d1fe39ebb6
Jie ren Tian <110873204@qq.com>
2018-05-06 22:36:26 +0800

    鲜花系统订单完成页ShoppingResult组件开发.

--- src/user/shoppingFinish/shoppingResult/ShoppingResult.jsx
+++ src/user/shoppingFinish/shoppingResult/ShoppingResult.jsx
@@ -3,6 +3,8 @@
  * Created by tianrenjie on 2018/5/6
  */
 import React, { PropTypes } from 'react';
+import { Row, Col, Icon } from 'antd';
+import ItemSelf from '../../itemList/itemList/itemSelf/ItemSelf';
 import './ShoppingResult.less';
 
 class ShoppingResult extends React.Component {
@@ -10,7 +12,65 @@ class ShoppingResult extends React.Component {
   render() {
     return (
       <div className="shoppingresult">
-        ...
+        <div className="shoppingresult-content">
+          <Row>
+            <Col>
+              <p className="shoppingresult-content-p">
+                <span className="shoppingresult-content-p-span">
+                  <Icon type="check-circle" />
+                </span>
+                您已完成付款！
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="shoppingresult-content-next">
+                <button className="shoppingresult-content-next-btn">返回首页</button>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3 className="shoppingresult-more">—&emsp;更多推荐&emsp;—</h3>
+              <div className="shoppingresult-list clearfix">
+                <ItemSelf
+                  item={{
+                    img: '',
+                    title: '携手一生',
+                    description: '11枝优质红玫瑰搭配绿叶，满天星，随机赠送2只小熊',
+                    nowPrice: '￥179',
+                    prePrice: '￥299',
+                    sales: '6751',
+                    comments: '8329',
+                  }}
+                />
+                <ItemSelf
+                  item={{
+                    img: '',
+                    title: '携手一生',
+                    description: '11枝优质红玫瑰搭配绿叶，满天星，随机赠送2只小熊',
+                    nowPrice: '￥179',
+                    prePrice: '￥299',
+                    sales: '6751',
+                    comments: '8329',
+                  }}
+                />
+                <ItemSelf
+                  item={{
+                    img: '',
+                    title: '携手一生',
+                    description: '11枝优质红玫瑰搭配绿叶，满天星，随机赠送2只小熊',
+                    nowPrice: '￥179',
+                    prePrice: '￥299',
+                    sales: '6751',
+                    comments: '8329',
+                  }}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
